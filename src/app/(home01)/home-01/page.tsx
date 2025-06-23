970b66a94e11a59cc2b3aecb8eb0508e00b6c84b
akhtarhssn <watchonlytutorial@gmail.com>
2025-06-23 14:01:14 +0600

    new home page setup

---
+++ src/app/(home01)/home-01/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage01 = () => {
+  return (
+    <div>
+      Home Page 01
+    </div>
+  )
+}
+
+export default HomePage01
---
+++ src/app/(home01)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LayoutHome01 = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            {/* header */}
+
+            {children}
+        </>
+    )
+}
+
+export default LayoutHome01
